91e152a34b8147d2e6536e91791ab0e8072b10c0
chilohwei <chilohwei@gmail.com>
2024-03-24 15:48:07 +0800

    update

--- RECENT.md
+++ RECENT.md
@@ -1,5 +1,5 @@
-* [第 168 期 - 想去现场](https://weekly.tw93.fun/posts/168-想去现场) - 2024-03-18
-* [第 167 期 - 碧空雪涛](https://weekly.tw93.fun/posts/167-碧空雪涛) - 2024-03-11
-* [第 166 期 - 猫晒太阳](https://weekly.tw93.fun/posts/166-猫晒太阳) - 2024-03-03
-* [第 165 期 - 年度榜单](https://weekly.tw93.fun/posts/165-年度榜单) - 2024-02-26
-* [第 164 期 - 又是一年](https://weekly.tw93.fun/posts/164-又是一年) - 2024-02-18
+* [第 06 期 - 接受一切](https://weekly.chiloh.cn/posts/06-接受一切) - 2023-12-18
+* [第 05 期 - 当下不杂](https://weekly.chiloh.cn/posts/05-当下不杂) - 2023-12-04
+* [第 04 期 - 本色示人](https://weekly.chiloh.cn/posts/04-本色示人) - 2023-11-26
+* [第 03 期 - 又值一岁](https://weekly.chiloh.cn/posts/03-又值一岁) - 2023-11-19
+* [第 02 期 - 看演唱会](https://weekly.chiloh.cn/posts/02-看演唱会) - 2023-11-15
--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def fetch_ci_time(filePath):
 
 if __name__ == "__main__":
   readmefile=open('README.md','w')
-  readmefile.write("# 潮流周刊\n\n> 记录工程师 Tw93 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/tw93/weekly/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
+  readmefile.write("# 奇洛周刊\n\n> 记录 Chiloh 的日常观察，欢迎订阅，也欢迎 [推荐](https://github.com/chilohwei/weekly/discussions/categories/ideas) 你的好东西，Fork 自用可见 [开发文档](https://github.com/chilohwei/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
   recentfile=open('RECENT.md','w')
 
   for root, dirs, filenames in os.walk('./src/pages/posts'):
@@ -22,7 +22,7 @@ if __name__ == "__main__":
       if name.endswith('.md'):
         filepath = urllib.parse.quote(name)
         oldTitle = name.split('.md')[0]
-        url   = 'https://weekly.tw93.fun/posts/' + oldTitle
+        url   = 'https://weekly.chiloh.cn/posts/' + oldTitle
         title = '第 ' + oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
         readmeMd= '* [{}]({})\n'.format(title, url)
         dateList = ["2022-10-10","2022-09-26","2022-09-12","2022-09-05","2022-08-29"]
